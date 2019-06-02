9c38b11c05d0cf5dd6f5d27926464a1c3c5eddb3
AW63926 <aw63926@gmail.com>
2019-06-02 10:07:43 -0400

    Added an example of exatly how to set up an Iterator class/Interface

---
+++ src/TheIterator.java
@@ -0,0 +1,23 @@
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+public class TheIterator implements Iterator<String> {
+	
+	
+	public boolean hasNext() {
+		return false;
+	}
+	
+	public void remove() {
+	}
+
+	@Override
+	public String next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
